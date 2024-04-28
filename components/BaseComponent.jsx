bb8cf14bc00e4b6416fb29478ccbfdd7736d6b19
hrshkshri <harshkeshari100@gmail.com>
2024-04-28 22:40:09 +0530

    checkpoint

--- components/BaseComponent.jsx
+++ components/BaseComponent.jsx
@@ -5,19 +5,19 @@ import { Box, Image, Img } from '@chakra-ui/react'
 const BaseComponent = ({ children }) => {
     return (
         <>
-            <Box className='bg-white rounded-3xl w-3/4 h-4/5'
+            <Box className='bg-white rounded-3xl w-full lg:w-3/4 h-full lg:h-4/5'
             >
                 <div className='p-5'>
                     <Image src='/logo.svg' alt='logo' width={150} />
                 </div>
                 <Box className="bg-white rounded-3xl container mx-auto flex flex-row justify-between items-center p-6 w-full h-3/4"
-                    bgImage="https://builds.contra.com/c138bbb2/assets/static/background.Dg9zT2Nv.webp" bgSize="contain" bgPosition="right" bgRepeat="no-repeat"
+                    // bgImage="https://builds.contra.com/c138bbb2/assets/static/background.Dg9zT2Nv.webp" bgSize="contain" bgPosition="right" bgRepeat="no-repeat"
                 >
-                    <div className="flex-1">
+                    <div className="flex-1 w-full">
                         {children}
                     </div>
-                    <Box className="flex-1 p-6" >
-                        <div className="flex flex-col items-center justify-center space-y-3">
+                    <Box className="lg:flex-1 p-6" >
+                        <div className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:space-y-3 hidden">
                             <BaseModal />
                             <div className='text-xs font-light'>
                                 JOIN TOP TEAMS HIRING ON CONTRA
